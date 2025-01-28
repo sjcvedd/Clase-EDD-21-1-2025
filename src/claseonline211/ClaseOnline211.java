7a463565d7c404ea880ade7ae8acd5bd491d8dc2
sjcvedd <scardenas.8865@unimar.edu.ve>
2025-01-27 20:18:08 -0400

    initial commit

---
+++ src/claseonline211/ClaseOnline211.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package claseonline211;
+
+/**
+ *
+ * @author Silver
+ */
+public class ClaseOnline211 {
+    
+    public static void main(String[] args) {
+        int a,b,c; // a,b,c : integer
+        a=1;//a:=1
+        b=2;//a:=1
+        c=1;//a:=1
+        /*if (a==c) { // a=b
+            System.out.println("verdadero");
+        }else{
+            System.out.println("falso");
+        }*/
+        /*if (a!=c) { // ab
+            System.out.println("verdadero");
+        }else{
+            System.out.println("falso");
+        }*/
+        /*switch (a) { //case a of
+            case 1:
+                
+                break;
+            case 2:
+                
+                break;
+            case 3:
+                
+                break;
+            default:
+                throw new AssertionError();
+        }*/
+        // i++ = i:=i+1 (pascal) = i= i+1 = i+=1
+        /*for (int i = 0; i < 10;i+=2) { // for i:=1 to 10 do
+            System.out.println(i);
+        }*/
+        /*int i=0;
+        while (i == 0){ // <--  while (i=0) -> while(true)
+            
+        }
+        */
+        /*
+        do { // repeat
+            
+        } while (false); //until
+        */
+        //System.out.println("Hola mundo");
+        // TODO code application logic here
+    }
+    
+}
